31deaef33713dbc87d85ec9274a586852ee44c53
Tomas Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2020-11-02 18:58:34 -0300

    Agrega endpoint de publicaciones

--- requester.js
+++ requester.js
@@ -1,1 +1,16 @@
-// TODO
+
+const PUBLICATIONS_ENDPOINT = "https://bookbnb5-publications.herokuapp.com/v1/publication"
+
+class Requester {
+    async publications()  {
+        let publications = await fetch(PUBLICATIONS_ENDPOINT)
+            .then(response => {
+                return response.json()
+            }).catch( error => {
+                return null
+            })
+        return publications
+    }
+}
+
+export { Requester }
