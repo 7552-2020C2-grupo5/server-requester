15a90ffc69ccc14c22514f8f5cafc84c377c1123
Ariel <vergara.ari@gmail.com>
2021-02-21 22:38:21 -0300

    Se modifican los endopoints de admins

--- endpoints/NewAdminEndpoint.js
+++ endpoints/NewAdminEndpoint.js
@@ -1,12 +1,12 @@
 import {Endpoint} from "./Endpoint.js";
-import {MIDDLEWARE_BASE_ENDPOINT} from "../requester";
+import {MIDDLEWARE_BASE_ENDPOINT, USERS_BASE_ENDPOINT} from "../requester";
 import {PostUserSuccessful} from "../responses/users/PostUserSuccessful";
 import {PostUserAlreadyRegistered} from "../responses/users/PostUserAlreadyRegistered";
 import {InvalidEmail} from "../responses/users/InvalidEmail";
 
 export class NewAdminEndpoint extends Endpoint {
     url() {
-        return MIDDLEWARE_BASE_ENDPOINT + '/admins/'
+        return USERS_BASE_ENDPOINT + '/admins'
     }
 
     ownResponses() {
